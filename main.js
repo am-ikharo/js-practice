b580163f54edc3a41a56616a81f63a20e480bc86
am-ikharo <85495558+am-ikharo@users.noreply.github.com>
2025-07-01 23:36:49 +0100

    shorter method

--- main.js
+++ main.js
@@ -36,8 +36,9 @@ const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 // })
 
 // console.log(canDrink);
+const canDrink = ages.filter(age => age >= 21 )
 
-
+console.log(canDrink);
 //map
 //sort
 //reduce

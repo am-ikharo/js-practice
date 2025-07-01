a9ff0a410c4375b69cc7457b528ac66b9e461787
am-ikharo <85495558+am-ikharo@users.noreply.github.com>
2025-07-01 22:53:01 +0100

    created an array of ages

--- main.js
+++ main.js
@@ -9,3 +9,11 @@ const companies = [
 {name: "Company Eight", category: "Technology", start: 1981, end: 2003},
 {name: "Company Nine", category: "Retail", start: 1981, end: 2003},
 ];
+
+const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
+
+// forEach
+//filter
+//map
+//sort
+//reduce

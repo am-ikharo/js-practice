2e94c81a65c7133bb50256f7f8f077cf0f7b72c5
am-ikharo <85495558+am-ikharo@users.noreply.github.com>
2025-07-01 23:27:27 +0100

    comment

--- main.js
+++ main.js
@@ -20,13 +20,12 @@ const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 // companies.forEach(company => {
 //     console.log(company[]);
 // })
-const canDrink = [];
-for(let i =0; i <= ages.length; i++){
-    if(ages[i] >= 21 ) {
-        canDrink.push(ages[i])
-    }
-    
-}
+// const canDrink = [];
+// for(let i =0; i <= ages.length; i++){
+//     if(ages[i] >= 21 ) {
+//         canDrink.push(ages[i])
+//     }
+// }
 
 console.log(canDrink);
 //filter

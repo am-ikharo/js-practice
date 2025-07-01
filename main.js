0b46a33cbdd5c183f8e98d910f3f69fb987b3d00
am-ikharo <85495558+am-ikharo@users.noreply.github.com>
2025-07-01 22:48:50 +0100

    created a array of companies

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 const companies = [
-
-]
+{name: "Company One", category: "Finance", start: 1981, end: 2003},
+{name: "Company Two", category: "Retail", start: 1981, end: 2003},
+{name: "Company Three", category: "Auto", start: 1981, end: 2003},
+{name: "Company Fpur", category: "Retail", start: 1981, end: 2003},
+{name: "Company Five", category: "Technology", start: 1981, end: 2003},
+{name: "Company Six", category: "Finance", start: 1981, end: 2003},
+{name: "Company Seven", category: "Auto", start: 1981, end: 2003},
+{name: "Company Eight", category: "Technology", start: 1981, end: 2003},
+{name: "Company Nine", category: "Retail", start: 1981, end: 2003},
+];

4be9491202e4ce7b75addba23946b266e01ab28f
am-ikharo <85495558+am-ikharo@users.noreply.github.com>
2025-07-02 23:55:04 +0100

    company

--- main.js
+++ main.js
@@ -48,9 +48,7 @@ const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 //map
  const lastedTenYears = companies.map(company =>
      `${company.name} [${company.start}, ${company.end}]`
-    ).filter(company => {
-        company.end - company.start > 10
-    })
+    )
 
  console.log(lastedTenYears);
  

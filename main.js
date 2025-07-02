5df44487ce4c1a5eaaaca57c3f743b8fb8e6dc03
am-ikharo <85495558+am-ikharo@users.noreply.github.com>
2025-07-02 23:59:28 +0100

    added category

--- main.js
+++ main.js
@@ -47,7 +47,7 @@ const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 // console.log(nameOfCompanies);
 //map
  const lastedTenYears = companies.map(company =>
-     `${company.name} [${company.start}, ${company.end}]`
+     `${company.category} [${company.start}, ${company.end}]`
     )
 
  console.log(lastedTenYears);
